064572b2718daf6f6c21a1a44f2ce0a9f4b46781
Nam1130Tx <69258625+Nam1130Tx@users.noreply.github.com>
2020-09-15 14:26:18 -0500

    Files added via upload
    First Javascript push

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+var y = "Howdy Partner!"  //creates the variable "y", and defines it as a string value   
+
+window.alert(y)  //Display's "y" in a alert window
+
+document.write("Tacos are great<br>")  //Display's the string text in ""
+
+
+var x = "<br>\'Salsa is spicy.\'&nbsphe said."  //Assigns the variable "x" a string value, and 
+var x = x.fontcolor("maroon");                 //Sets the font color of "x" to maroon
+
+document.write(x)                              //displays the variable "x"
+
+document.write("<br><br>Red salsa tends to be sweeter," //concentrates the three different strings
++ "than green salsa,"
++ "which is spicy.<br><br>");
+
+var k = "Roasted tomatoes and peppers," + "give the salsa a smokier flavor.<br>" + "<br>"; //Concentrates variables
+var k = k.fontcolor("red");
+document.write(k)
+
+var peppers = "heat", hot = "Ghost Pepper", medium = "Jalapeno Pepper", mild = "Poblano Pepper"; //sets multiple variables
+var hot = hot.fontcolor("orange");
+document.write(hot)
+
+var jap = "<br><br>Jalapeno\'s are green.<br><br>";  //Sets the font color of the variable to green.
+var jap = jap.fontcolor("green");
+document.write(jap)
+
+document.write(3 + 3); //Displays the sum of the expression
+
+
+
