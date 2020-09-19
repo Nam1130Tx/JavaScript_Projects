5a5b9677144c90c22b4262e47aadb3da5b8cd2f4
Nam1130Tx <69258625+Nam1130Tx@users.noreply.github.com>
2020-09-19 12:31:30 -0500

    Files added via upload
    TicTacToe Project files added.

---
+++ TicTacToe/js/tictactoe.js
@@ -0,0 +1,124 @@
+let activePlayer = "X";                                                     //This is basically setting the player as X, the computer will be O.
+let selectedSquares = [];
+function placeXOrO(squareNumber) {
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById(squareNumber);
+        if (activePlayer === 'X') {                                        //This is putting the right marker on the screen
+            select.style.backgroundImage = 'url("images/camo.png")';
+        } else {
+            select.style.backgroundImage = 'url("images/ninja.png")';
+        }
+        selectedSquares.push(squareNumber + activePlayer);
+        checkWinConditions();
+        if (activePlayer === "X") {
+            activePlayer = 'O';
+        } else {
+            activePlayer = 'X';
+        }
+        audio('./media/ding.wav');                                        //Puts the placement audio in.
+        if(activePlayer === 'O') {
+            disableClick();
+            setTimeout(function () { computersTurn(); }, 1000);
+        }
+        return true;
+    }
+    function computersTurn() {                                            //iniates the computers turn, if player hasnt won yet.
+        let success = false;
+        let pickASquare;
+        while(!success) {
+            pickASquare = String(Math.floor(Math.random() * 9));
+            if (placeXOrO(pickASquare)) {
+                placeXOrO(pickASquare);
+                success = true;
+            };
+        }
+    }
+}
+
+function checkWinConditions() {                                                     //function that looks for if the game is won.
+    if      (arrayIncludes('OX', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
+    else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
+    else if (selectedSquares.length >= 9) {
+        audio('./media/tiedgame.mp3');                                               //plays the tied game sfx if all markers are placed, and no win is found.
+        setTimeout(function () { resetGame();} , 1000);
+    }
+    function arrayIncludes(squareA, squareB, squareC) {
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+        if (a === true && b=== true && c === true) {return true;}
+    }
+}
+
+function disableClick() {                                                            //Disables the player turn, when the computer is playing
+    body.style.pointerEvents = 'none';
+    setTimeout(function() {body.style.pointerEvents = 'auto';}, 1000);
+}
+
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    audio.play();
+}
+
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {                          //This function puts the line start and end point in the canvas element.
+    const c = canvas.getContext('2d') ;
+    let x1 = coordX1,
+        y1 = coordY1,
+        x2 = coordX2,
+        y2 = coordY2,
+        x = x1,
+        y = y1;
+
+    function animateLineDrawing() {                                                //Here the line is actually drawn in the canvas element.
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 225, 33, 0.8)';
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y -= 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+    function clear() {                                                             //This clears the line after the game.
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+    disableClick();                                                                //Disables new clicks and plays the win sound.
+    audio('./media/wingame.wav');
+    animateLineDrawing();
+    setTimeout(function() {clear(); resetGame(); }, 1000);
+}
+
+function resetGame() {                                                             //resets the game
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i));
+        square.style.backgroundImage = '';
+    }
+    selectedSquares = [];
+}
